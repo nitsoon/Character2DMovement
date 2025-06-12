5027128161842ff991ae2248fac4b9fa00722ba9
nitson <mhdsilver8@gmail.com>
2025-06-12 05:17:54 -0700

    Update Character2DMovement.cs

--- Character2DMovement.cs
+++ Character2DMovement.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class Character2DMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
 
     private Rigidbody2D rb;
     private Animator animator;
     private Vector2 movement;
+    private bool isGrounded;
 
     void Start()
     {
@@ -22,18 +27,21 @@ public class Character2DMovement : MonoBehaviour
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
         if (animator != null)
         {
             animator.SetFloat("MoveX", movement.x);
-            animator.SetFloat("MoveY", movement.y);
-            animator.SetFloat("Speed", movement.sqrMagnitude);
+            animator.SetFloat("Speed", Mathf.Abs(movement.x));
+            animator.SetBool("isJumping", !isGrounded);
         }
     }
-
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+       
+        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
     }
 }
